132e184dd75eaecadf330b5de48fe3a9f5a6cc71
Aruming <0430kar@naver.com>
2023-03-08 02:40:49 +0900

    POST /member/join

--- src/main/java/com/ex/rest_api_test/controller/MemberController.java
+++ src/main/java/com/ex/rest_api_test/controller/MemberController.java
@@ -1,13 +1,12 @@
 package com.ex.rest_api_test.controller;
 
+import com.ex.rest_api_test.dto.MemberJoinDto;
 import com.ex.rest_api_test.entity.Member;
 import com.ex.rest_api_test.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +20,24 @@ public class MemberController {
     public ResponseEntity list(){
         List<Member> memberList = memberService.findAll();
 
-        return new ResponseEntity<>(memberList, null, HttpStatus.OK);
+        return new ResponseEntity(memberList, null, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity detail(@PathVariable Long id){
+        Member member = memberService.findById(id).orElse(null);
+
+        if(member == null){
+            return new ResponseEntity(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity(member, null, HttpStatus.OK);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity join(@RequestBody MemberJoinDto memberJoinDto){
+        Member newMember = memberService.join(memberJoinDto.getName(), memberJoinDto.getAge());
+
+        return new ResponseEntity(newMember, null, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/ex/rest_api_test/service/MemberService.java
+++ src/main/java/com/ex/rest_api_test/service/MemberService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,9 @@ public class MemberService {
 
     public List<Member> findAll() {
         return memberRepository.findAllByOrderByIdAsc();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return memberRepository.findById(id);
     }
 }
